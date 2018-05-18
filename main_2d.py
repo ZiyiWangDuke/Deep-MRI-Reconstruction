b61190a3004932144d9d189aa100a23d856b2c46
Ziyi Wang <ziyiwang@ip-172-16-40-189.ec2.internal>
2018-05-18 14:51:01 -0400

    finished trainning script

--- main_2d.py
+++ main_2d.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 
-import os
+import os, pdb
 import time
 import numpy as np
 import theano
@@ -31,7 +31,9 @@ def prep_input(im, acc=4):
     gauss_ivar: float - controls the undersampling rate.
                         higher the value, more undersampling
     """
-    mask = cs.cartesian_mask(im.shape, acc, sample_n=8)
+    # mask = cs.cartesian_mask(im.shape, acc, sample_n=8)
+    mask = cs.var_2d_mask(shape=im.shape,acc=acc)
+
     im_und, k_und = cs.undersample(im, mask, centred=False, norm='ortho')
     im_gnd_l = to_lasagne_format(im)
     im_und_l = to_lasagne_format(im_und)
@@ -69,13 +71,11 @@ def create_dummy_data():
     return train, validate, test
 
 
-def compile_fn(network, net_config, args):
+def compile_fn(network, net_config, base_lr,l2):
     """
     Create Training function and validation function
     """
     # Hyper-parameters
-    base_lr = float(args.lr[0])
-    l2 = float(args.l2[0])
 
     # Theano variables
     input_var = net_config['input'].input_var
@@ -111,36 +111,38 @@ def compile_fn(network, net_config, args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_epoch', metavar='int', nargs=1, default=['10'],
-                        help='number of epochs')
-    parser.add_argument('--batch_size', metavar='int', nargs=1, default=['10'],
-                        help='batch size')
-    parser.add_argument('--lr', metavar='float', nargs=1,
-                        default=['0.001'], help='initial learning rate')
-    parser.add_argument('--l2', metavar='float', nargs=1,
-                        default=['1e-6'], help='l2 regularisation')
-    parser.add_argument('--acceleration_factor', metavar='float', nargs=1,
-                        default=['4.0'],
-                        help='Acceleration factor for k-space sampling')
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--num_epoch', metavar='int', nargs=1, default=['10'],
+    #                     help='number of epochs')
+    # parser.add_argument('--batch_size', metavar='int', nargs=1, default=['10'],
+                        # help='batch size')
+    # parser.add_argument('--lr', metavar='float', nargs=1,
+    #                     default=['0.001'], help='initial learning rate')
+    # parser.add_argument('--l2', metavar='float', nargs=1,
+                        # default=['1e-6'], help='l2 regularisation')
+    # parser.add_argument('--acceleration_factor', metavar='float', nargs=1,
+    #                     default=['4.0'],
+    #                     help='Acceleration factor for k-space sampling')
     # parser.add_argument('--gauss_ivar', metavar='float', nargs=1,
     #                     default=['0.0015'],
     #                     help='Sensitivity for Gaussian Distribution which'
     #                     'decides the undersampling rate of the Cartesian mask')
-    parser.add_argument('--debug', action='store_true', help='debug mode')
-    parser.add_argument('--savefig', action='store_true',default=True,
-                        help='Save output images and masks')
+    # parser.add_argument('--debug', action='store_true', help='debug mode')
+    # parser.add_argument('--savefig', action='store_true',default=True,
+    #                     help='Save output images and masks')
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
     # Project config
-    model_name = 'd2_c2'
-    #gauss_ivar = float(args.gauss_ivar[0])  # undersampling rate
-    acc = float(args.acceleration_factor[0])  # undersampling rate
-    num_epoch = int(args.num_epoch[0])
-    batch_size = int(args.batch_size[0])
+    model_name = 'd5_c5'
+
+    num_epoch = 20
+    batch_size = 5
+    base_lr = 0.001
+    l2 = 1e-6
+
     Nx, Ny = 128, 128
-    save_fig = args.savefig
+    save_fig = True
     save_every = 5
 
     # Configure directory info
@@ -156,49 +158,57 @@ if __name__ == '__main__':
     # Load D5-C5 with pretrained params
     net_config, net,  = build_d5_c5(input_shape)
     # D5-C5 with pre-trained parameters
-    with np.load('./models/pretrained/d5_c5.npz') as f:
-        param_values = [f['arr_{0}'.format(i)] for i in range(len(f.files))]
-        lasagne.layers.set_all_param_values(net, param_values)
+    # with np.load('./models/pretrained/d5_c5.npz') as f:
+    #     param_values = [f['arr_{0}'.format(i)] for i in range(len(f.files))]
+    #     lasagne.layers.set_all_param_values(net, param_values)
 
     # Compute acceleration rate
-    dummy_mask = cs.cartesian_mask((10, Nx, Ny), acc, sample_n=8)
-    sample_und_factor = cs.undersampling_rate(dummy_mask)
-    import pdb; pdb.set_trace()
-    print('Undersampling Rate: {:.2f}'.format(sample_und_factor))
+    # dummy_mask = cs.cartesian_mask((10, Nx, Ny), acc, sample_n=8)
+    # sample_und_factor = cs.undersampling_rate(dummy_mask)
+    # import pdb; pdb.set_trace()
+    # print('Undersampling Rate: {:.2f}'.format(sample_und_factor))
 
     # Compile function
-    train_fn, val_fn = compile_fn(net, net_config, args)
+    train_fn, val_fn = compile_fn(net, net_config, base_lr,l2)
 
 
     # Create dataset
     train, validate, test = create_dummy_data()
+    train = train[:10,:,:]
+    validate = validate[:10,:,:]
+    test = test[:10,:,:]
+    # data shape num*128*128
 
     print('Start Training...')
     for epoch in xrange(num_epoch):
         t_start = time.time()
-        print('ZW: finished epoch:'+str(epoch))
+        print('ZW: starting epoch:'+str(epoch))
+
         # Training
         train_err = 0
         train_batches = 0
+        k=0
         for im in iterate_minibatch(train, batch_size, shuffle=True):
+            # generate acc randomly from 1 to 6
+            print(k)
+            k+=1
+            acc = np.random.random(1)*5+1
+
             im_und, k_und, mask, im_gnd = prep_input(im, acc=acc)
             err = train_fn(im_und, mask, k_und, im_gnd)[0]
             train_err += err
             train_batches += 1
 
-            if args.debug and train_batches == 20:
-                break
-
         validate_err = 0
         validate_batches = 0
         for im in iterate_minibatch(validate, batch_size, shuffle=False):
+            # generate acc randomly from 1 to 6
+            acc = np.random.random(1)*5+1
+
             im_und, k_und, mask, im_gnd = prep_input(im, acc=acc)
             err, pred = val_fn(im_und, mask, k_und, im_gnd)
             validate_err += err
             validate_batches += 1
-
-            if args.debug and validate_batches == 20:
-                break
 
         vis = []
         test_err = 0
@@ -206,6 +216,9 @@ if __name__ == '__main__':
         test_psnr = 0
         test_batches = 0
         for im in iterate_minibatch(test, batch_size, shuffle=False):
+            # generate acc randomly from 1 to 6
+            acc = np.random.random(1)*5+1
+
             im_und, k_und, mask, im_gnd = prep_input(im, acc=acc)
 
             err, pred = val_fn(im_und, mask, k_und, im_gnd)
@@ -223,9 +236,6 @@ if __name__ == '__main__':
                             from_lasagne_format(pred)[0],
                             from_lasagne_format(im_und)[0],
                             from_lasagne_format(mask, mask=True)[0]))
-
-            if args.debug and test_batches == 20:
-                break
 
         t_end = time.time()
 
@@ -253,12 +263,13 @@ if __name__ == '__main__':
                                abs(np.concatenate([und_i, pred_i,
                                                    im_i, im_i - pred_i], 1)),
                                cmap='gray')
+                               
                     plt.imsave(join(save_dir, 'mask{0}.png'.format(i)), mask_i,
                                cmap='gray')
                     i += 1
 
             name = '%s_epoch_%d.npz' % (model_name, epoch)
-            # np.savez(join(save_dir, name),
-                     # *lasagne.layers.get_all_param_values(net))
+            np.savez(join(save_dir, name),
+                     *lasagne.layers.get_all_param_values(net))
             print('model parameters saved at %s' % join(os.getcwd(), name))
             print('')
